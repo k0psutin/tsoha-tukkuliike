cc86eb4f54bd5bf085de71f55315f8c14a6a99c7
k0psutin <mr.gravez@gmail.com>
2020-09-16 19:36:18 +0300

    Move to /db_interface

--- db.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from os import getenv
-from app import app
-
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["SQLALCHEMY_ECHO"] = True
-db = SQLAlchemy(app)
