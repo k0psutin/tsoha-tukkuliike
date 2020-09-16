3ab056b1e031c803acb0c8bddbc2238e5da88ccd
k0psutin <mr.gravez@gmail.com>
2020-09-16 19:31:51 +0300

    Refactor application
    Added bunch of imports at the end of app.py, so my newly organized
    routes folder are imported correctly.

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ from os import getenv
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
-import routes
+import routes.auth_router
+import routes.logistic_router
+import routes.collector_router
+import routes.sales_router
+import routes.buyer_router
+import routes.user_router
