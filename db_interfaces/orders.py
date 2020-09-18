2078090bd622518c52158839af1f2a05befa39bc
k0psutin <mr.gravez@gmail.com>
2020-09-18 12:50:01 +0300

    Add functions for updating the order
    Added functions:
    
    remove_item_from_sale_order,
    add_item_to_sale_order,
    update_sale_order_item_qty,
    delete_order_by_order_id

--- db_interfaces/orders.py
+++ db_interfaces/orders.py
@@ -2,9 +2,11 @@ from random import randint
 
 from flask.helpers import flash
 from db_interfaces.db import db
+import db_interfaces.item as item
+import db_interfaces.users as users
 
 
-def get_sale_order(order_id):  # TODO query not working properly
+def get_sale_order(order_id):
     sql = """SELECT orders.item_id,
                     items.itemname, 
 	                orders.qty AS required, 
@@ -64,6 +66,73 @@ def get_order_total(order_id):
     sql = "SELECT SUM(orders.price) FROM orders WHERE order_id = :order_id"
     result = db.session.execute(sql, {"order_id": order_id})
     return result.fetchone()[0]
+
+
+def remove_item_from_sale_order(item_id, order_id):
+    sql = "DELETE FROM orders WHERE item_id = :item_id AND order_id = :order_id"
+    db.session.execute(sql, {"item_id": item_id, "order_id": order_id})
+    db.session.commit()
+
+
+def add_item_to_sale_order(order_id, company_id, item_id, qty):
+    sql = "SELECT qty FROM orders WHERE order_id = :order_id AND item_id = :item_id AND company_id = :company_id"
+    result = db.session.execute(
+        sql, {"order_id": order_id, "item_id": item_id, "company_id": company_id})
+    sale = result.fetchone()
+    qty = int(qty)
+
+    print(sale)
+
+    if sale == None:
+        sql = """INSERT INTO orders (order_id, company_id, item_id, supply, orderDate, dispatchDate, qty, price, user_id) 
+             VALUES (:order_id, :company_id, :item_id, FALSE, NOW(), NOW() + INTERVAL '1 DAY', :qty, :price, :user_id);"""
+        price = round((float(qty) * float(item.get_price(item_id))*1.0), 2)
+
+    else:
+        sql = "UPDATE orders SET price = :price, qty = :qty WHERE company_id = :company_id AND item_id = :item_id"
+        qty += int(sale[0])
+        price = round(float(item.get_price(item_id)) * float(qty), 2)
+
+    user_id = users.get_user_id()
+
+    db.session.execute(
+        sql, {"order_id": order_id, "company_id": company_id, "item_id": item_id, "qty": qty, "price": price, "user_id": user_id})
+    db.session.commit()
+
+
+def update_sale_order_item_qty(order_id, item_list, company_id, qty_list):
+    sql = "UPDATE orders SET price = :price, qty = :qty WHERE company_id = :company_id AND item_id = :item_id AND order_id = :order_id"
+
+    inserts = []
+
+    for i in range(len(qty_list)):
+
+        if qty_list[i] == '':
+            continue
+
+        if qty_list[i] == '0':
+            remove_item_from_sale_order(item_list[i], order_id)
+            continue
+
+        else:
+            price = round(float(item.get_price(
+                item_list[i])) * float(qty_list[i]), 2)
+
+            insert = {"order_id": order_id,
+                      "company_id": company_id,
+                      "item_id": item_list[i],
+                      "qty": qty_list[i],
+                      "price": price}
+
+            inserts.append(insert)
+
+    if len(inserts) == 0:
+        return
+
+    print(inserts)
+
+    db.session.execute(sql, inserts)
+    db.session.commit()
 
 
 def create_sale_order(company_id, order_list, qty_list, price_list, user_id):
@@ -164,11 +233,18 @@ def create_supply_order(company_id_list, item_list, qty_list, price_list, user_i
     db.session.commit()
 
 
+def delete_order_by_order_id(order_id):
+    sql = "DELETE FROM orders WHERE order_id = :order_id"
+    db.session.execute(sql, {"order_id": order_id})
+    flash("Order %s removed successfully" % order_id)
+
+
 def get_company_by_order_id(order_id, supply=False):
     sql = """SELECT companies.compname, 
                     companies.address, 
                     companies.email, 
-                    companies.country 
+                    companies.country,
+                    companies.company_id 
             FROM orders 
             INNER JOIN companies ON (companies.company_id = orders.company_id)
             WHERE order_id=:order_id AND supply = :supply"""
