c1916e7ed2b71e44b21f833acfef318f9534325b
k0psutin <mr.gravez@gmail.com>
2020-10-03 22:43:02 +0300

    Add func for order item removal

--- static/js/utils.js
+++ static/js/utils.js
@@ -7,3 +7,8 @@ function confirmShipment(order_id) {
   var conf = confirm('Close and mark order ' + order_id + ' as collected?')
   return conf
 }
+
+function confirmDeleteItem(item_name) {
+    var conf = confirm('Remove item ' + item_name + ' from order?')
+    return conf
+}
