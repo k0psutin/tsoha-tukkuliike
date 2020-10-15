64a9620b381ebb0143068c5f381348262ad4b9f7
k0psutin <mr.gravez@gmail.com>
2020-10-15 12:50:49 +0300

    Test data

---
+++ db/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('asiakas','pbkdf2:sha256:150000$gIYwSqNi$2075a6d901645193ddfe6957286cc0bae266f3565a8ee4181b2f0f746de8c0b3',1,17);
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('varasto','pbkdf2:sha256:150000$7igSzBKb$34bb633f05bf3ea49ec78d0312a282a111e7e9e0aaa66cfa4cd8fea1a4ef849f',2, null);
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('keraily','pbkdf2:sha256:150000$g2bsEmcq$a618b67daddb86e4ecceee3736b7fd8989c3aaa05aeea9f43f0062ca0f85ba66',3, null);
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('myynti','pbkdf2:sha256:150000$MtU2PXXi$3ff60b700f60b4ad3b99e721da67d44f8eca5d67dac126f07b32c25cbe31e684',4, null);
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('osto','pbkdf2:sha256:150000$7Bjb98Le$98f8a7623e5806528bd8aae6841ae499695af945a9b68c4e334e24489b105deb',5, null);
+INSERT INTO users (username, pswd, auth_lvl, company_id) VALUES ('kontrolleri','pbkdf2:sha256:150000$F1lxBXs6$aac3395ce18a9ab3080c7b730c0437abe1a8ac8b46fb3f6199bab16bad789171',6, null);
